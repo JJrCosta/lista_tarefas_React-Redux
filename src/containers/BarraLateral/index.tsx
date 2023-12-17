41f0c56f7e1d7555f186fe5ea8699341379a108f
JJrCosta <jrpercosta@gmail.com>
2023-12-17 13:08:15 -0300

    Add Filter parameters at BarraLateral

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,3 +1,20 @@
-const BarraLateral = () => <aside>Barra Lateral</aside>
+import FiltroCard from '../../components/FiltroCard'
+import * as S from './syles'
+
+const BarraLateral = () => (
+  <S.Aside>
+    <div>
+      <S.Campo type="text" placeholder="Buscar" />
+      <S.Filtros>
+        <FiltroCard legenda="pendentes" contador={1} />
+        <FiltroCard legenda="concluidas" contador={2} />
+        <FiltroCard legenda="urgentes" contador={0} />
+        <FiltroCard legenda="importantes" contador={4} />
+        <FiltroCard legenda="normal" contador={1} />
+        <FiltroCard legenda="todas" contador={8} ativo />
+      </S.Filtros>
+    </div>
+  </S.Aside>
+)
 
 export default BarraLateral
